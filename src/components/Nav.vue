a51eedf846fd204d68220c09589d3d71a7e188b4
lirongyue <761780862@qq.com>
2020-04-15 23:59:49 +0800

    点击å更新路由颜色

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,17 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       记账
       <Icon name="money" />
     </router-link>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       标签
       <Icon name="label" />
 
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       统计
       <Icon name="statistics" />
     </router-link>
@@ -46,6 +46,9 @@
     .icon{
       width:32px;
       height: 32px;
+    }
+    >.item.selected{
+      color:red;
     }
   }
 </style>
