5f7cfb967805930ecdfb5d88acf2aaa25eaf0e82
lirongyue <761780862@qq.com>
2020-04-15 23:51:36 +0800

    导航样式确定

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       记账
       <Icon name="money" />
     </router-link>
-    |
-    <router-link to="/labels">
+
+    <router-link to="/labels" class="item">
       标签
       <Icon name="label" />
 
     </router-link>
-    |
-    <router-link to="/statistics">
+
+    <router-link to="/statistics" class="item">
       统计
       <Icon name="statistics" />
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -29,7 +29,23 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid red
+  nav {
+    /*border: 1px solid red;*/
+    font-size:12px;
+    display: flex;
+    flex-direction: row;
+    box-shadow: 0 0 2px rgba(0,0,0,0.25);
+    > .item{
+      padding: 2px 0;
+      width:33.3333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column-reverse;
+    }
+    .icon{
+      width:32px;
+      height: 32px;
+    }
   }
 </style>
