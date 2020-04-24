17be4a2a75c6a7bc92a041036684dc589affeda5
lirongyue <761780862@qq.com>
2020-04-24 16:12:43 +0800

    模块化的store，删除了models

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import router from './router';
 import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
-import tagListModel from '@/models/tagListModel';
-import recordListModel from '@/models/recordListModel';
+
 
 Vue.config.productionTip = false;
 Vue.component('Nav', Nav);
